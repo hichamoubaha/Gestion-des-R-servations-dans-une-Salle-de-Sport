dd0f1a0033724713c9a3939c9fae39f4b59b28de
hichamoubaha <oubahahicham7@gmail.com>
2024-12-11 18:47:35 +0100

    creez tableau membres

--- script.sql
+++ script.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE sport1;
 USE sport1;
+
+-- Creation du tableau membres
+CREATE TABLE membres (
+    id_membre INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(50),
+    prenom VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    telephone VARCHAR(15)
+);
